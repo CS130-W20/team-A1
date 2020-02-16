f505c3892508801748f36a7fd1c4550b82ecd613
Omar Tleimat <otleimat@gmail.com>
2020-02-15 19:47:50 -0800

    Added options for headless and dev for Chrome webdriver (#20)

--- server/query.py
+++ server/query.py
@@ -2,12 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 # follow the first instructions from https://selenium-python.readthedocs.io/installation.html#downloading-python-bindings-for-selenium
 # Download the chrome driver and put it inside /usr/local/bin
 
 # creates webdriver class
-driver = webdriver.Chrome()
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--headless")
+
+driver = webdriver.Chrome(options=options)
 driver.get("http://www.google.com")
 
 def get_query_from_goog(query):
