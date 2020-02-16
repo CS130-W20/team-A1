937ef5408d91768bcfd000cb8162a95a17f1aed8
Omar Tleimat <otleimat@gmail.com>
2020-02-16 14:09:32 -0800

    added socketio flask server

---
+++ server/lobby.py
@@ -0,0 +1,22 @@
+import os
+
+from flask import request, jsonify
+
+from flask import Flask, render_template, url_for
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from collections import defaultdict
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+@app.route("/")
+def index():
+    return "Hey"
+
+
+if __name__ == '__main__':
+	socketio.run(app)
+	
+	
+	
