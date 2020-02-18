907f05c46a5e90cfde66878b2c4ef72a86d40501
Omar Tleimat <otleimat@gmail.com>
2020-02-18 13:18:11 -0800

    Added code for lobby creation

--- server/lobby.py
+++ server/lobby.py
@@ -3,34 +3,46 @@ import os
 from flask import request, jsonify
 from flask import session
 from flask_socketio import rooms
-
 from flask import Flask, render_template, url_for
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_socketio import send, emit
 from collections import defaultdict
+import random
+
 import requests
 
-game_rooms = []
+lobby_names = ["Dwarf", "Bree", "Dale", "Drúedain", "Dunlendings", "Easterling", "Haradrim", "Hobbit", "Maiar", "Orc", "Quenya", "Rohirrim", "Sindarin"]
+game_rooms = {}
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
-socketio = SocketIO(app,cors_allowed_origins='*')
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('my event')
 def handle_my_custom_event(json):
     print('received json: ' + str(json))
 
 
-@socketio.on('join')
+@socketio.on('create_room')
 def on_join(data):
     print(data)
     username = data['username']
     room = data['room']
-    join_room(room)
-    if room not in game_rooms:
-        game_rooms.append(room)
-    print(game_rooms)
-    print(rooms())
-    send("hey whats up", room=room)
+
+    lobby_num = random.randint(50,8000)
+    lobby = "{0}{1}".format(random.choice(lobby_names), lobby_num)
+    if lobby not in game_rooms:
+        game_rooms[lobby] = {
+            "room_name": lobby,
+            "host": username,
+            "prompter": username,
+            "clients": [username],
+            "round_num": 0
+        }
+        join_room(lobby)
+        print(game_rooms)
+        print(rooms())
+        emit('lobby_created', game_rooms[lobby], room=lobby)
+    
 
 @app.route("/")
 def index():
