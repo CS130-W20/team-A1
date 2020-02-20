b5f977a19843abb2091dfcb14790526752add384
= <Jmitchell1991@gmail.com>
2020-02-20 15:08:16 -0800

    [feat] - adds more logic to test query

--- server/test_query.py
+++ server/test_query.py
@@ -7,6 +7,8 @@ def test_query_internal():
     """
     Here we test the internal functionality of our query Api 
     that is not the proxy
+    @expects query "the cats" to get parsed and send to chrome Selenium plugin 
+    @expects the returned parsed response to have length greater than 0
     """
     query = get_query_from_goog("the cats")
     assert(len(query) > 0)
@@ -16,6 +18,7 @@ def test_query_internal():
 def client(request):
     """
     Creates client fixture which can be used to access Flask API
+    @returns: instance of testable flask app
     """
     test_client = app.test_client()
 
@@ -25,6 +28,10 @@ def client(request):
     return test_client
 
 def get_json(client, url):
+    """
+    Utilizes Flask client to access GET request.
+    @param{url}: at '/query/?query=<queryhere>' url
+    """
     response = client.get(url)
     return response
 
@@ -33,6 +40,8 @@ def test_query_proxy(client):
     Tests lobby proxy server and its ability to ping query and extract 
     answers from query using the endpoint
     Tests: Response HTTP Status code, and length of data
+    Expected: Response as byte type with status code 200
+    Expected Response converted to ArrayList with length greater than 4
     """
     response = get_json(client, '/query/?query=cat')
     assert(response.status_code==200)
